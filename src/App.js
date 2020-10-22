4a63afd5ca297bd459fd85e6dae01e5139c62cb6
CarolLiu <carol.gugu@hotmail.com>
2020-10-22 14:00:34 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    // console.log("Was clicked!");
+    this.setState({
+      persons: [
+        { name: "Jenn", age: 27 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 26 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
